9d5f4d6ff10bc47ee75e7ec21708077d32777460
bradripple <bradripple@gmail.com>
2021-10-01 14:55:35 -0600

    Update: gadded submit button and got newProgress to save to progress

--- src/components/ExerciseDetails.js
+++ src/components/ExerciseDetails.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 import ProgressForm from "./ProgressForm";
 import RepsTracker from "./trackers/RepsTracker";
 import SetsTracker from "./trackers/SetsTracker";
 import WeightTracker from "./trackers/WeightTracker";
 import DistanceTracker from "./trackers/DistanceTracker";
 import DurationTracker from "./trackers/DurationTracker";
+
+const { REACT_APP_SERVER_URL } = process.env;
 
 const ExerciseDetails = (props) => {
   const [duration, setDuration] = useState(0);
@@ -14,28 +18,28 @@ const ExerciseDetails = (props) => {
   const [sets, setSets] = useState(0);
 
   const increaseSets = () => {
-      const newSetsNumber = sets >= 30 ? 10 : sets + 1;
-      setSets(newSetsNumber)
-  }
+    const newSetsNumber = sets >= 30 ? 10 : sets + 1;
+    setSets(newSetsNumber);
+  };
   const clearSets = () => {
-      setSets(0)
-  }
+    setSets(0);
+  };
 
   const increaseReps = () => {
-      const newRepsNumber = reps >= 30 ? 10 : reps + 1;
-      setReps(newRepsNumber)
-  }
+    const newRepsNumber = reps >= 30 ? 10 : reps + 1;
+    setReps(newRepsNumber);
+  };
   const clearReps = () => {
-      setReps(0)
-  }
+    setReps(0);
+  };
 
   const increaseWeight = () => {
-      const newWeightNumber = weight >= 800 ? 10 : weight + 5;
-      setWeight(newWeightNumber)
-  }
+    const newWeightNumber = weight >= 800 ? 10 : weight + 5;
+    setWeight(newWeightNumber);
+  };
   const clearWeight = () => {
-      setWeight(0)
-  }
+    setWeight(0);
+  };
 
   const increaseDistance = () => {
     const newDistanceNumber = distance >= 10 ? 10 : distance + 1;
@@ -49,11 +53,25 @@ const ExerciseDetails = (props) => {
     const newDurationNumber = duration >= 1000 ? 10 : duration + 1;
     setDuration(newDurationNumber);
   };
-
   const clearDuration = () => {
     setDuration(0);
   };
-  console.log("details props", props);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    console.log("we hit the handlesubmit for the form");
+
+      const newProgress = { duration, weight, reps, sets, distance, exerciseId: props._id };
+      axios
+        .post(`${REACT_APP_SERVER_URL}/progress`, newProgress)
+        .then((response) => {
+          console.log("===> Yay, new exercise");
+          console.log('exercise response', response);
+          // setRedirect(true);
+        })
+        .catch((error) => console.log("===> Error in Signup", error));
+    
+  };
 
   return (
     <div className="card">
@@ -66,13 +84,22 @@ const ExerciseDetails = (props) => {
           <p className="java">Reps: {props.repsGoal}</p>
           <p className="java">Sets: {props.setsGoal}</p>
           <p className="java">Distance: {props.distanceGoal}</p>
+        <button onClick={handleSubmit} >Save Progress</button>
         </div>
-        <RepsTracker reps={reps} increaseReps={increaseReps} clearReps={clearReps} />
-        <SetsTracker sets={sets} increaseSets={increaseSets} clearSets={clearSets} />
-        <WeightTracker 
-        weight={weight}
-        increaseWeight={increaseWeight}
-        clearWeight={clearWeight}
+        <RepsTracker
+          reps={reps}
+          increaseReps={increaseReps}
+          clearReps={clearReps}
+        />
+        <SetsTracker
+          sets={sets}
+          increaseSets={increaseSets}
+          clearSets={clearSets}
+        />
+        <WeightTracker
+          weight={weight}
+          increaseWeight={increaseWeight}
+          clearWeight={clearWeight}
         />
         <DistanceTracker
           distance={distance}
